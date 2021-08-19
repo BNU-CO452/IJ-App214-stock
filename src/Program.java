63d665501fb8e308214d9aca682baf6e9666f33a
Derek Peacock <derek.peacock@yahoo.com>
2021-08-19 19:13:43 +0100

    Initial create

---
+++ src/Program.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Program here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Program
+{
+    private static StockApp app;
+
+    /**
+     * This class contains the main method which
+     * is called first.
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        app = new StockApp();
+        app.run();
+    }
+}
